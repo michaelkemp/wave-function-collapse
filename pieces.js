7acaa0cc4e74288d8e4c8dc64f80dc877f27f1a5
michaelkemp <michael.5kemps@gmail.com>
2022-04-26 13:19:51 -0600

    kludgy

---
+++ pieces.js
@@ -0,0 +1,121 @@
+const sDim = 6;
+const pSize = 56;
+let PIECES = [];
+let EDGES = []; 
+
+class Piece {
+    constructor(image, index, uE, rE, dE, lE) {
+        this.image = image;
+        this.index = index;
+        this.uE = uE;
+        this.rE = rE;
+        this.dE = dE;
+        this.lE = lE;
+        this.u = [];
+        this.r = [];
+        this.d = [];
+        this.l = [];
+    }
+    display(x, y, imgSize) {
+        image(this.image, x, y, imgSize, imgSize);
+    }
+}
+
+function loadPieces() {
+    for(let y=0; y<sDim; ++y) {
+        for(let x=0; x<sDim; ++x) {
+            img = createImage(pSize, pSize);
+            img.copy(srcImage, x * pSize, y * pSize, pSize, pSize, 0, 0, pSize, pSize);
+            let i = (y * sDim) + x;
+            let [uE, rE, dE, lE] = edges(img);
+            PIECES[i] = new Piece(img, i, uE, rE, dE, lE);
+        }
+    }
+    // Write Edge Matches into Pieces
+    for(let i=0; i<PIECES.length; ++i) {
+        let testU = PIECES[i].uE;
+        let testR = PIECES[i].rE;
+        let testD = PIECES[i].dE;
+        let testL = PIECES[i].lE
+        let matchU = [];
+        let matchR = [];
+        let matchD = [];
+        let matchL = [];
+        for(let j=0; j<PIECES.length; ++j) {
+            if (testU == PIECES[j].dE) matchU.push(parseInt(j));
+            if (testR == PIECES[j].lE) matchR.push(parseInt(j));
+            if (testD == PIECES[j].uE) matchD.push(parseInt(j));
+            if (testL == PIECES[j].rE) matchL.push(parseInt(j));
+        }
+        PIECES[i].u = [...matchU];
+        PIECES[i].r = [...matchR];
+        PIECES[i].d = [...matchD];
+        PIECES[i].l = [...matchL];
+    }
+}
+
+// Calculate an Edge Index based on Pixel Color of Each Edge
+function edges(image) {
+    image.loadPixels();
+    let x, y, ind, r, g, b, a;
+    let uE, rE, dE, lE;
+    let up="";
+    let right="";
+    let down="";
+    let left="";
+    for(let i=0 ;i<pSize; ++i) {
+        //up
+        y=0; x=i;
+        ind = (x + y*pSize)*4
+        r = image.pixels[ind+0];
+        g = image.pixels[ind+1];
+        b = image.pixels[ind+2];
+        a = image.pixels[ind+3];
+        up+=r+"-"+g+"-"+b+"-"
+        //down
+        y=pSize-1; x=i;
+        ind = (x + y*pSize)*4
+        r = image.pixels[ind+0];
+        g = image.pixels[ind+1];
+        b = image.pixels[ind+2];
+        a = image.pixels[ind+3];
+        down+=r+"-"+g+"-"+b+"-"
+        //left
+        x=0; y=i;
+        ind = (x + y*pSize)*4
+        r = image.pixels[ind+0];
+        g = image.pixels[ind+1];
+        b = image.pixels[ind+2];
+        a = image.pixels[ind+3];
+        left+=r+"-"+g+"-"+b+"-"
+        //right
+        x=pSize-1; y=i;
+        ind = (x + y*pSize)*4
+        r = image.pixels[ind+0];
+        g = image.pixels[ind+1];
+        b = image.pixels[ind+2];
+        a = image.pixels[ind+3];
+        right+=r+"-"+g+"-"+b+"-"
+    }
+    uE = EDGES.indexOf(up);
+    if (uE == -1) {
+        EDGES.push(up);
+        uE = EDGES.length-1;
+    }
+    rE = EDGES.indexOf(right);
+    if (rE == -1) {
+        EDGES.push(right);
+        rE = EDGES.length-1;
+    }
+    dE = EDGES.indexOf(down);
+    if (dE == -1) {
+        EDGES.push(down);
+        dE = EDGES.length-1;
+    }
+    lE = EDGES.indexOf(left);
+    if (lE == -1) {
+        EDGES.push(left);
+        lE = EDGES.length-1;
+    }
+    return [uE,rE,dE,lE];
+}
---
+++ sketch.js
@@ -0,0 +1,115 @@
+// Preload Source Image
+let srcImage;
+
+function preload() {
+  srcImage = loadImage('circuit2.png');
+}
+
+const indexOfAll = (arr, val) => arr.reduce((acc, el, i) => (el === val ? [...acc, i] : acc), []);
+const DIM = 50;
+const TILE = 7;
+const WIDE = DIM * TILE;
+const PCOUNT = 36;
+let ALL = [];
+let tiles = [];
+let reductions = [];
+
+function setup() {
+  loadPieces();
+  for(let i=0; i<PIECES.length; ++i) {
+    ALL[i] = i;
+  }
+  loadTiles();
+  for (let i = 0; i < DIM * DIM; ++i) {
+    reductions[i] = PCOUNT;
+  }
+  createCanvas(WIDE, WIDE);
+  background(240);
+}
+
+
+function draw() {
+  let min = Math.min(...reductions);
+  if (min == 100) {
+    noLoop();
+    return;
+  } else {
+    let tmp = indexOfAll(reductions, min);
+    coll = tmp[Math.floor(Math.random() * tmp.length)];
+  }
+  tiles[coll].collapse();
+  reductions[coll] = 100;
+  propagate(coll);
+
+  PIECES[tiles[coll].tile].display( tiles[coll].x, tiles[coll].y, tiles[coll].wide );
+}
+
+function urdl(i) {
+  let u = i >= DIM ? i - DIM : -1;
+  let r = (i + 1) % DIM > 0 ? i + 1 : -1;
+  let d = i < DIM * DIM - DIM ? i + DIM : -1;
+  let l = i % DIM > 0 ? i - 1 : -1;
+  return [u, r, d, l];
+}
+
+function reduce(i) {
+  const [up, rt, dn, lt] = urdl(i);
+  let FILTER = [...ALL];
+  if (up >= 0 && tiles[up].collapsed) {
+    FILTER = ALL.filter((x) => PIECES[tiles[up].tile].d.includes(x));
+  }
+  if (rt >= 0 && tiles[rt].collapsed) {
+    FILTER = FILTER.filter((x) => PIECES[tiles[rt].tile].l.includes(x));
+  }
+  if (dn >= 0 && tiles[dn].collapsed) {
+    FILTER = FILTER.filter((x) => PIECES[tiles[dn].tile].u.includes(x));
+  }
+  if (lt >= 0 && tiles[lt].collapsed) {
+    FILTER = FILTER.filter((x) => PIECES[tiles[lt].tile].r.includes(x));
+  }
+  tiles[i].available = [...FILTER];
+  reductions[i] = FILTER.length;
+}
+
+function propagate(i) {
+  const [up, rt, dn, lt] = urdl(i);
+  let test = tiles[i].tile;
+  if ((up >= 0) && (!tiles[up].collapsed)) reduce(up);
+  if ((rt >= 0) && (!tiles[rt].collapsed)) reduce(rt);
+  if ((dn >= 0) && (!tiles[dn].collapsed)) reduce(dn);
+  if ((lt >= 0) && (!tiles[lt].collapsed)) reduce(lt);
+}
+
+class Tile {
+  constructor(i, x, y, width) {
+    this.index = i;
+    this.x = x * width;
+    this.y = y * width;
+    this.wide = width;
+    this.available = [...ALL];
+    this.collapsed = false;
+    this.tile = -1;
+  }
+  collapse() {
+    if (this.available.length > 0) {
+      this.tile = this.available[
+        Math.floor(Math.random() * this.available.length)
+      ];
+    } else {
+      // KLUDGE
+      console.log("K:", this.index);
+      this.tile = 1;
+    }
+    this.available = [...[this.tile]];
+    this.collapsed = true;
+  }
+}
+
+function loadTiles() {
+  for (let i = 0; i < DIM * DIM; ++i) {
+    let x = i % DIM;
+    let y = floor(i / DIM);
+    tiles[i] = new Tile(i, x, y, TILE);
+  }
+}
+
